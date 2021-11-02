e58846c3fd998162aa7431f0b7e65a847341d5ad
Matt Clifford <mattc863@hotmail.com>
2021-11-02 16:50:34 +0000

    tests for monitor start up and logging joules

---
+++ energy_monitor/tests/test_monitor.py
@@ -0,0 +1,34 @@
+'''
+test functions to analyse energy monitoring wrapper to intel power gadget
+'''
+# Author: Matt Clifford <matt.clifford@bristol.ac.uk>
+
+import pytest
+import platform
+import energy_monitor
+
+def test_standard_input():
+    # standard input args
+    monitor = energy_monitor.monitor()
+    if platform.system() == 'Windows':
+        assert monitor.bin == '"C:\\Program Files\\Intel\\Power Gadget 3.6\\IntelPowerGadget.exe"'
+
+def test_non_standard_input():
+    # non standard file location
+    monitor = energy_monitor.monitor(non_standard_location='/bin/loc.bin')
+    assert monitor.bin == '/bin/loc.bin'
+
+def test_input_error():
+    # # test error for non string
+    args=[True, 0.1]
+    for arg in args:
+        with pytest.raises(ValueError) as val_error:
+            monitor = energy_monitor.monitor(non_standard_location=arg)
+        assert str(val_error.value) == 'argument: non_standard_location={arg} needs to be type str'.format(arg=repr(arg))
+
+def test_joules_value():
+    monitor = energy_monitor.monitor()
+    monitor.start()
+    energy_monitor.utils.dummy_compute(2)
+    monitor.stop()
+    assert type(monitor.joules) == float
