ae6a700f51a37f0d3096c8dce6a7f8423139eac4
Matt Clifford <mattc863@hotmail.com>
2021-11-12 12:46:55 +0000

    example csv files for dev purposes

--- energy_monitor/data-examples/create_data.py
+++ energy_monitor/data-examples/create_data.py
@@ -1,10 +1,19 @@
 '''
 create some example data for dev usage
+
+useage: will save to './example.csv' (pwd)
 '''
 # Author: Matt Clifford <matt.clifford@bristol.ac.uk>
-import energy_monitor
+from energy_monitor import monitor, utils, train_mnist
 
-mon1 = monitor(remove_background_energy=True)
-mon1.start()
-utils.dummy_compute(20)
-mon1.stop()
+logfile = './example.csv'
+names = ['Dummy Compute', 'Train Mnist']
+funcs = [utils.dummy_compute, train_mnist.main]
+for i in range(len(names)):
+    for background in [False, True]:
+        mon = monitor(name=names[i],
+                       remove_background_energy=background,
+                       log_filepath=logfile)
+        mon.start()
+        funcs[i]()
+        mon.stop()
