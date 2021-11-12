0c8d1d643237f769989fb6d1bc91b0da48d4bff0
Matt Clifford <mattc863@hotmail.com>
2021-11-12 13:00:03 +0000

    logging test now testing properly

--- energy_monitor/tests/test_logging.py
+++ energy_monitor/tests/test_logging.py
@@ -12,6 +12,15 @@ import os
 def test_log_data():
     dictionary = {'Time': 10.23, 'Energy 1': 100.23, 'Energy 2': 54.92}
     csv_filepath = os.path.join(os.getcwd(), 'energy_monitor', 'tests', 'data_examples', 'test_write.csv')
+    if os.path.isfile(csv_filepath):
+        os.remove(csv_filepath)
     energy_monitor.utils.log_data(csv_filepath, dictionary)
 
-    # TODO: test this with a csv_reader
+    result = energy_monitor.utils.read_results(csv_filepath)
+    assert result['Time'][-1] == 10.23
+    assert result['Energy 1'][-1] == 100.23
+    assert result['Energy 2'][-1] == 54.92
+    os.remove(csv_filepath)
+
+if __name__ == '__main__':
+    test_log_data()
