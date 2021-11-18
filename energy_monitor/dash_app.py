a570c6b676f0750cad379bf55ec10ace43a8a9f2
maurock <maurocomi92@gmail.com>
2021-11-18 14:43:51 +0000

    added plot2

--- energy_monitor/dash_app.py
+++ energy_monitor/dash_app.py
@@ -18,13 +18,12 @@ from dash.dependencies import Input, Output
 if __name__ == "__main__":
     # Instantiate Dash app
     app = dash.Dash()     
-    print(os.getcwd())
+
     # Get data
     #data = energy_monitor.utils.read_results('energy_monitor/data-examples/example.csv')
     data = pd.read_csv('energy_monitor/data-examples/example.csv')
     names_unique = np.unique(data['name'].values)
     labels_dropdown = [{'label' : name, 'value':name } for name in names_unique]
-    print(labels_dropdown)
 
     # Dash histogram
     app.layout = html.Div(children=[
@@ -40,21 +39,32 @@ if __name__ == "__main__":
             value=names_unique,    # default
             multi=True
         ),        
-        dcc.Graph(id='cumulative_consumption')
+        dcc.Graph(id='plot_1'),
+        dcc.Graph(id='plot_2')
     ])
     
     @app.callback(
-    Output('cumulative_consumption', 'figure'),
+    Output('plot_1', 'figure'),
     Input('multi_selector', 'value')
     )
-    def update_output(names):
+    def update_plot1(names):
         #values = data[data==names]
         df = data[data['name'].isin(names)]
         fig = px.bar(df, x='name', y='cumulative_ia', title="Cumulative Energy",
                     labels={'cumulative_ia':'Cumulative Energy [J]', 'name':'Test name'}
         )
-        print(names)
+        return fig
 
+    @app.callback(
+    Output('plot_2', 'figure'),
+    Input('multi_selector', 'value')
+    )
+    def update_plot2(names):
+        df = data
+        df['date'] = data['date'].apply(lambda x: x[:10])
+        fig = px.bar(data, x='date', y='cumulative_ia', title="Cumulative Energy", color="name",
+                    labels={'cumulative_ia':'Cumulative Energy [J]', 'date':'Date'}
+        )
         return fig
     
     app.run_server(debug=True)   
