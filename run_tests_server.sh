bcf0bb86f95803036a43f0fbb3798b30e0dca327
Matt Clifford <mattc863@hotmail.com>
2021-11-19 12:58:40 +0000

    new tests

---
+++ run_tests_server.sh
@@ -0,0 +1,10 @@
+# make sure conda is accessable
+CONDA_BASE=$(conda info --base)
+source $CONDA_BASE/etc/profile.d/conda.sh
+
+# activate virtual env
+VENV=energy_monitor
+conda activate $VENV
+
+# run tests
+pytest energy_monitor/tests/misc
