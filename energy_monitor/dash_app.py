a46af7355e3e2e18cc5181d0a4faa07729e89b72
mattclifford1 <mc15445@bristol.ac.uk>
2021-12-07 10:15:31 +0000

    Update dash_app.py

--- energy_monitor/dash_app.py
+++ energy_monitor/dash_app.py
@@ -23,7 +23,7 @@ import plotly.graph_objects as go
 import webbrowser
 
 
-def run(csv_file='energy_monitor/data-examples/example.csv'):
+def run(csv_file=os.path.join(os.path.dirname(os.path.realpath(__file__))'data-examples/example.csv')):
     external_stylesheets = [
         "https://fonts.googleapis.com/css2?family=Nunito&display=swap",
         dbc.themes.MATERIA
